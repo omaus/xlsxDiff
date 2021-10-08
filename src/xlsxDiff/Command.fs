d65328abe402b84ce828306da5e0bb31fdec1143
omaus <omaus@bio.uni-kl.de>
2021-10-08 18:04:44 +0200

    Add command module for Spectre.Console.Cli

---
+++ src/xlsxDiff/Command.fs
@@ -0,0 +1,17 @@
+﻿module Command
+
+open Spectre.Console.Cli
+
+let args = [|"loL"|]
+
+[<AbstractClass; Sealed>]
+type FileSizeCommandSettings =
+    inherit CommandSettings
+    
+
+[<AbstractClass; Sealed>]
+type FileSizeCommand =
+    inherit Command<FileSizeCommandSettings>
+
+let app = CommandApp<FileSizeCommand>()
+app.Run(args)
